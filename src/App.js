08b7bc3ff39d69f8aa3d947edb7858fd60b9b00e
unknown <jasser1heni@gmail.com>
2020-12-08 09:20:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import logo from './logo.svg';
+import Imagess from './component/image';
 import './App.css';
-import  * as ReactBootstrap from "react-bootstrap";
-import { Navbar, From, Nav,NavDropdown,FormControl,Button } from "react-bootstrap";
+import { Navbar, Form, Nav,NavDropdown,FormControl,Button } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
-      <ReactBootstrap.Navbar bg="light" expand="lg">
-  <ReactBootstrap.Navbar.Brand href="#home">React-Bootstrap</ReactBootstrap.Navbar.Brand>
-  <ReactBootstrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <ReactBootstrap.Navbar.Collapse id="basic-navbar-nav">
-    <ReactBootstrap.Nav className="mr-auto">
-      <ReactBootstrap.Nav.Link href="#home">Home</ReactBootstrap.Nav.Link>
-      <ReactBootstrap.Nav.Link href="#link">Link</ReactBootstrap.Nav.Link>
-      <ReactBootstrap.NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <ReactBootstrap.NavDropdown.Item href="#action/3.1">Action</ReactBootstrap.NavDropdown.Item>
-        <ReactBootstrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootstrap.NavDropdown.Item>
-        <ReactBootstrap.NavDropdown.Item href="#action/3.3">Something</ReactBootstrap.NavDropdown.Item>
-        <ReactBootstrap.NavDropdown.Divider />
-        <ReactBootstrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootstrap.NavDropdown.Item>
-      </ReactBootstrap.NavDropdown>
-    </ReactBootstrap.Nav>
-    <ReactBootstrap.Form inline>
+      <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home" className="sky" >skydiving</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
       <Button variant="outline-success">Search</Button>
-    </ReactBootstrap.Form>
-  </ReactBootstrap.Navbar.Collapse>
-</ReactBootstrap.Navbar>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+   <Imagess/>
     </div>
   );
-}
 
+
+}
 export default App;
